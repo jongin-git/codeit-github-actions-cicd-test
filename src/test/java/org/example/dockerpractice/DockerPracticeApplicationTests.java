a395c373644fa61dc4506e5245a3a57807732f51
jongin-git <jongin33@gmail.com>
2026-04-03 15:01:19 +0900

    수정2

--- src/test/java/org/example/dockerpractice/DockerPracticeApplicationTests.java
+++ src/test/java/org/example/dockerpractice/DockerPracticeApplicationTests.java
@@ -2,11 +2,20 @@ package org.example.dockerpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class DockerPracticeApplicationTests {
+
+    @MockBean
+    private S3Client s3Client;
+
+    @MockBean
+    private S3Presigner s3Presigner;
 
     @Test
     void contextLoads() {
